c684a066ad432e33ae3b1ee71c666cae4eea8551
Pedro Urbina <pedro.urbina@gmail.com>
2015-09-03 13:55:55 -0400

    user name capitalization assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 	before_save {self.email = email.downcase}
+	before_save {
+		capray = []
+		self.name.split.each do |x|
+			capray << x.capitalize
+		end
+		self.name = capray.join(" ")
+	}
+
 	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, length: {minimum: 1, maximum: 100}, presence: true
 	validates :password, length: {minimum: 6}, presence: true
