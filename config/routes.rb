e6b3e932f0eb8e5b895d312289b352e2977e879c
Pedro Urbina <pedro.urbina@gmail.com>
2015-09-12 17:23:17 -0400

    Labels working!

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :labels, only: [:show]
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
