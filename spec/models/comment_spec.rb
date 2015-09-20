02c2109db92774ba151c402861da33c4fc81e472
Pedro Urbina <pedro.urbina@gmail.com>
2015-09-20 10:33:27 -0400

    callbacks added

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Comment, type: :model do
-	let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
-  	let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
-  	let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
-  	let(:comment) {Comment.create!(body:'Comment Body', post: post, user: user)}
-  	it {should belong_to(:post)}
-  	it {should belong_to(:user)}
-  	it {should validate_presence_of(:body)}
-  	it {should validate_length_of(:body).is_at_least(5)}
+  let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
+  let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
+  let(:comment) {Comment.create!(body:'Comment Body', post: post, user: user)}
+  it {should belong_to(:post)}
+	it {should belong_to(:user)}
+  it {should validate_presence_of(:body)}
+  it {should validate_length_of(:body).is_at_least(5)}
 
-  	context "attributes" do
-  		it "should respond to body" do
-  			expect(comment).to respond_to(:body)
-  		end
+  context "attributes" do
+  	it "should respond to body" do
+  		expect(comment).to respond_to(:body)
   	end
+  end
+  describe "after_create" do
+    before do
+      @another_comment = Comment.new(body: 'Comment body', post: post, user: user)
+    end
+    it "sends an email to users who have favorited the post" do
+      favorite = user.favorites.create(post: post)
+      expect(FavoriteMailer).to receive(:new_comment).with(user, post, @another_comment).and_return(double(deliver_now: true))
+      @another_comment.save
+    end
+    it "does not send emails to users who havent favorited the post" do
+      expect(FavoriteMailer).not_to receive(:new_comment)
+      @another_comment.save
+    end
+  end
 end
