d71d50c82c099097136791a1eb7cb593b449244d
Pedro Urbina <pedro.urbina@gmail.com>
2015-09-29 13:13:31 -0400

    assignment completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+	def user_has_comments?(user)
+		if !@user.comments.nil?
+			render @user.comments
+		else
+			"#{user.name} has not submitted any posts yet."
+		end
+	end
 end
