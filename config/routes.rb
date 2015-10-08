97c3d14cced78b5960742f731a568faf178cc442
Pedro Urbina <pedro.urbina@gmail.com>
2015-10-08 14:52:27 -0400

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   get 'welcome/faq'
 
   root to: 'welcome#index'
-
+  
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
